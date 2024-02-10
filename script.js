58ff24c9878af7f5be032c1e5d440fa2bde12fc6
salve0609 <salve0609@gmail.com>
2024-02-10 18:40:41 +0530

    add

--- script.js
+++ script.js
@@ -7,10 +7,17 @@
 //  download visual studio code : https://code.visualstudio.com/download
 // Step 4: 
 // install node js
-// Step 5: install visual studio code
-// Step 6: check node version : node --version
-// Step 7: create folder : JavaScript 
-// Step 8: folder open in visual studio code
-// Step 9: create one file : script.js
-// Step 10: write code: console.log("Hello World!")
-// Step 11 : node script.js
+// Step 5: 
+// install visual studio code
+// Step 6: 
+//check node version : node --version
+// Step 7: 
+//create folder : JavaScript 
+// Step 8: 
+//folder open in visual studio code
+// Step 9: 
+//create one file : script.js
+// Step 10:
+// write code: console.log("Hello World!")
+// Step 11 :
+// node script.js
